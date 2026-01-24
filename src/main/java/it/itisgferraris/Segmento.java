cfff358b5acd0beca35854836432bf822fb2a2f2
Giu27 <Caruso.giuseppe09@hotmail.com>
2026-01-24 14:03:48 +0100

    Inizio lavori al segmento

--- src/main/java/it/itisgferraris/Segmento.java
+++ src/main/java/it/itisgferraris/Segmento.java
@@ -4,5 +4,18 @@ public class Segmento {
     private Punto inizio;
     private Punto fine;
 
-    
+    public Segmento(Punto inizio, Punto fine) {
+        this.inizio = inizio;
+        this.fine = fine;
+    }
+
+    public Segmento(double[] coordInizio, double[] coordFine){
+        inizio = new Punto(coordInizio);
+        fine = new Punto(coordFine);
+    }
+
+    public Segmento(int[] coordInizio, int[] coordFine){
+        inizio = new Punto(coordInizio);
+        fine = new Punto(coordFine);
+    }
 }
