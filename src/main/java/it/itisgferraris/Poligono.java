3c91d0a4d31e471a6a24df091a7af455e14c35f6
Giu27 <Caruso.giuseppe09@hotmail.com>
2026-01-25 09:55:40 +0100

    Nuovi metodi per poligono

--- src/main/java/it/itisgferraris/Poligono.java
+++ src/main/java/it/itisgferraris/Poligono.java
@@ -4,6 +4,17 @@ public abstract class Poligono {
     protected int numLati;
     protected Segmento[] lati;
 
+    public Poligono(int numLati, Punto[] vertici) throws Exception{
+        if (numLati < 3) {
+            throw new Exception("Troppi pochi vertici!");
+        }
+        this.numLati = numLati;
+        if (vertici.length != this.numLati) {
+            throw new Exception("Troppi vertici!");
+        }
+
+    }
+
     public int getNumLati() {
         return numLati;
     }
@@ -24,7 +35,38 @@ public abstract class Poligono {
         return somma;
     }
 
+    public boolean isRegolare(){
+        Segmento base = lati[0];
+        double angle = base.calcolaAngolo(lati[1]);
+        for (int i = 0; i < lati.length - 1; i++){
+            if (lati[i].calcolaLunghezza() != base.calcolaLunghezza()) {
+                return false;
+            }
+            if (lati[i].calcolaAngolo(lati[i + 1]) != angle) {
+                return false;
+            }
+        }
+
+        if (lati[-1].calcolaLunghezza() != base.calcolaLunghezza() || lati[-1].calcolaAngolo(base) != angle) {
+            return false;
+        }
+
+        return true;
+    }
+
     public abstract double calcolaArea();
 
-    protected abstract boolean validaLati(Segmento[] lati);
+    public abstract String classifica();
+
+    protected boolean validaVertici(Punto[] vertici) { //Controlla che dei vertici non si sovrappongano
+        for (int i = 0; i < numLati - 1; i++) {
+            for (int j = i + 1; j < numLati; j++) {
+                if (vertici[i].equals(vertici[j])) {
+                    return false;
+                }
+            }
+        }
+
+        return true;
+    }   
 }
